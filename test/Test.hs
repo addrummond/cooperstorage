206031a11b20cdb3ee8ec73d7a59027e84a30c2e
Alex Drummond <a.d.drummond@gmail.com>
2018-12-06 21:33:32 +0000

    Tweak Stack setup

--- test/Test.hs
+++ test/Test.hs
@@ -10,8 +10,6 @@ module Main where
 import Cooper (HasTrace(TraceOf), Simple, Val(Val), apply, lift, retrieve, run, store)
 import ExampleModel (Denotations(..), denotations, model, withModel)
 
-main = return ()
-
 test =
     let
         Denotations{ boy, smokes, every, some, likes, john } = withModel model denotations
@@ -19,3 +17,6 @@ test =
         --run (retrieve (store (apply every boy)))
         run (retrieve (apply (apply likes (store (apply every boy))) john))
         --run (apply (apply some boy) smokes)
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
