723950976abc5589019a4e94eb93cad132494e89
Alex Drummond <a.d.drummond@gmail.com>
2018-12-06 13:08:37 +0000

    Milestone

--- Main.hs
+++ Main.hs
@@ -2,18 +2,14 @@
 {-# language FlexibleInstances #-}
 {-# language TypeFamilies #-}
 {-# language GADTs #-}
+{-# language NamedFieldPuns #-}
 {-# language PolyKinds #-}
 module Main where
 
 main = return ()
 
-data Individual = John | Tom | Bill | Jane
-
 class HasTrace a where
     type TraceOf a :: *
-
-instance HasTrace ((Individual -> Bool) -> Bool) where
-    type TraceOf ((Individual -> Bool) -> Bool) = Individual
 
 data BinTree a = Empty | Leaf a | Branch (BinTree a) (BinTree a)
 
@@ -46,7 +42,11 @@ hListFirst :: HList a -> First a
 hListFirst HNil = ()
 hListFirst (HCons a xs) = a
 
-data Val s a = Val s a
+data Val s a where
+    Val :: HList s -> (HList a -> b) -> Val (HList s) ((HList a) -> b)
+
+lift :: a -> Val (HList 'Nil) (HList 'Nil -> a)
+lift v = Val HNil (\_ -> v)
 
 store
     :: Val (HList 'Nil) (HList 'Nil -> a)
@@ -85,7 +85,84 @@ apply (Val store1 f1) (Val store2 f2) =
                 (f1 p1) (f2 p2)
         )
 
-every :: Val (HList 'Nil) (HList 'Nil -> (Individual -> Bool) -> Bool)
+run :: Val (HList 'Nil) (HList 'Nil -> a) -> a
+run (Val _ f) = f HNil
+
+data Individual = John | Tom | Bill | Jane deriving Eq
+
+instance HasTrace ((Individual -> Bool) -> Bool) where
+    type TraceOf ((Individual -> Bool) -> Bool) = Individual
+
+data Model = Model
+    { individuals :: [Individual]
+    , boys        :: [Individual]
+    , girls       :: [Individual]
+    , smokers     :: [Individual]
+    , dancers     :: [Individual]
+    , likings     :: [(Individual, Individual)]
+    , detestings  :: [(Individual, Individual)]
+    }
+
+-- Id and Domain can be used to fake a type-level (* -> *) identity function.
+-- This is a technique used in the type-functions package.
+data Id a = Id a
+type family Domain a where
+    Domain (Id a) = a
+    Domain a = a
+
+data Denotations f = Denotations
+    { boy     :: Domain (f (Individual -> Bool))
+    , girl    :: Domain (f (Individual -> Bool))
+    , smokes  :: Domain (f (Individual -> Bool))
+    , dances  :: Domain (f (Individual -> Bool))
+    , likes   :: Domain (f (Individual -> Individual -> Bool))
+    , detests :: Domain (f (Individual -> Individual -> Bool))
+    , every   :: Domain (f ((Individual -> Bool) -> ((Individual -> Bool) -> Bool)))
+    , some    :: Domain (f ((Individual -> Bool) -> ((Individual -> Bool) -> Bool)))
+    }
+
+denotations :: Denotations ((->) Model)
+denotations = Denotations
+    { boy     = \Model{ boys } x -> x `elem` boys
+    , girl    = \Model{ girls } x -> x `elem` girls
+    , smokes  = \Model{ smokers } x -> x `elem` smokers
+    , dances  = \Model{ dancers } x -> x `elem` dancers
+    , likes   = \Model{ likings } x y -> (x,y) `elem` likings
+    , detests = \Model{ detestings } x y -> (x,y) `elem` detestings
+    , every   = \Model{ individuals } u v -> all (\x -> not (u x) || v x ) individuals
+    , some    = \Model{ individuals } u v -> any (\x -> u x && v x) individuals
+    }
+
+withModel :: Model -> Denotations ((->) Model) -> Denotations Id
+withModel m Denotations{ boy, girl, smokes, dances, likes, detests, every, some } = Denotations
+    { boy = boy m   
+    , girl = girl m
+    , smokes = smokes m
+    , dances = dances m
+    , likes = likes m
+    , detests = detests m
+    , every = every m
+    , some = some m
+    }
+
+exampleModel :: Model
+exampleModel = Model
+    { individuals = [John, Tom, Bill, Jane]
+    , boys        = [John, Tom, Bill]
+    , girls       = [Jane]
+    , smokers     = [John, Jane]
+    , dancers     = [John, Bill]
+    , likings     = [(John, Tom), (Bill, Jane)]
+    , detestings  = [(Jane, Tom)]
+    }
+
+test =
+    let
+        Denotations{ boy, smokes, every, some } = withModel exampleModel denotations
+    in
+        run (apply (apply (lift some) (lift boy)) (lift smokes))
+
+{-every :: Val (HList 'Nil) (HList 'Nil -> (Individual -> Bool) -> Bool)
 every = Val HNil (\_ _ -> True)
 
 smokes :: Val (HList 'Nil) (HList 'Nil -> Individual -> Bool)
@@ -97,7 +174,4 @@ likes = Val HNil (\_ _ _ -> True)
 john :: Val (HList 'Nil) (HList 'Nil -> Individual)
 john = Val HNil (\_ -> John)
 
-test = retrieve (apply (apply likes (store every)) john)
-
-run :: Val (HList 'Nil) (HList 'Nil -> a) -> a
-run (Val _ f) = f HNil
+test = retrieve (apply (apply likes (store every)) john)-}
